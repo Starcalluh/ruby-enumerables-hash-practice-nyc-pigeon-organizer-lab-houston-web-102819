7431205e0150cb733cb9e337e5a2ae63327e50a4
George <starcalluh@live.com>
2019-10-26 18:14:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-  data.each do |k , v|
-    hash = {}
-    puts k 
-    puts v
-  end
-  v.each do |k1, v1|
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   data.each do |k , v|
+#     hash = {}
+#     puts k 
+#     puts v
+#   end
+#   #v.each do |k1, v1|
     
     
-  return data
-end
+# # return data
+# #end
